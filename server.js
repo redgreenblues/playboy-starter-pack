22328d55d9307367dba8e3d1f34a6646f6399a00
teppy90 <jared.tai90@gmail.com>
2020-07-29 17:11:48 +0800

    Bare bones login authentication. Not tested. Waiting for front end repo setup.

--- server.js
+++ server.js
@@ -1,23 +1,100 @@
+// Imports
+
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const User = require("./user");
+
+// Database
+
+mongoose.connect(
+  "mongodb+srv://yuejia:yuejia@playboy-app.r7mrt.mongodb.net/test?authSource=admin&replicaSet=atlas-tsr2b5-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Mongoose Is Connected");
+  }
+);
+
+
 
 const app = express();
 require('./db');
-const MemesRouter = require('./routes/memesRouter');
+
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000", // <-- location of the react app we are connecting to
+    credentials: true,
+  })
+);
+
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig")(passport);
 
 // Routes
-app.use('/app', MemesRouter);
 
+app.use('/app', MemesRouter);
+const MemesRouter = require('./routes/memesRouter');
+
+app.post("/login", (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+      if (err) throw err;
+      if (!user) res.send("No User Exists");
+      else {
+        req.logIn(user, (err) => {
+          if (err) throw err;
+          res.send("Successfully Authenticated");
+          console.log(req.user);
+        });
+      }
+    })(req, res, next);
+  });
+  app.post("/register", (req, res) => {
+    User.findOne({ username: req.body.username }, async (err, doc) => {
+      if (err) throw err;
+      if (doc) res.send("User Already Exists");
+      if (!doc) {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  
+        const newUser = new User({
+          username: req.body.username,
+          password: hashedPassword,
+        });
+        await newUser.save();
+        res.send("User Created");
+      }
+    });
+  });
+  app.get("/user", (req, res) => {
+    res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it. 
+    //can be used for the rest of the app
+  });
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
