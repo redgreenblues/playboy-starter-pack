6041fc5e03235ff5dc4d7fac22a6aad8dbf7743b
Javier Fong <fong.kayyuen@gmail.com>
2020-07-31 21:11:37 +0800

    Edited cors origin

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:8000" || 'https://playboy-starter-pack-backend.herokuapp.com/app', // <-- location of the react app we are connecting to
+    origin: "http://localhost:8000" || process.env.MONGODB_URI, // <-- location of the react app we are connecting to
     credentials: true,
   })
 );
