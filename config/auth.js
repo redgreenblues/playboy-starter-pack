7568efb7429390cb328ccdd68f45ae3d1ee3ef6e
Javier Fong <fong.kayyuen@gmail.com>
2020-08-01 20:07:11 +0800

    edit auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
     ensureAuthenticated (req, res, next) {
         if (req.isAuthenticated()) return next();
-        else res.redirect('/login');
+        else res.redirect('/');
     }
 }
