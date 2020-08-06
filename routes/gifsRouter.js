32080bcbc18b181e1ca56ffeb64ab77464b8bf4f
YJ Lee <leeyuejia@gmail.com>
2020-08-06 23:06:38 +0800

    comment section

--- routes/gifsRouter.js
+++ routes/gifsRouter.js
@@ -6,6 +6,9 @@ const GifsController = require('../controllers/gifsController');
 
 
 router.post('/gif', ensureAuthenticated, GifsController.createGif);
+// create comment route
+router.post('/gif/:id/comment', ensureAuthenticated, GifsController.createGifComment);
+
 router.get('/gifs', ensureAuthenticated, GifsController.getGifs);
 router.get('/gif/:id', ensureAuthenticated, GifsController.getGifById);
 router.get('/gifs/:username', ensureAuthenticated, GifsController.getGifsByUsername);
